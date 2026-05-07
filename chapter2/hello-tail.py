7b5574e68b68df70c0ac74b1854f270af3552420
bababa <pijonsan@gmail.com>
2026-05-07 18:06:29 +0900

    RAW_TRACEPOINT_PROBEを使用した関数の定義に変更

--- chapter2/hello-tail.py
+++ chapter2/hello-tail.py
@@ -5,7 +5,7 @@ import time
 program = r"""
 BPF_HASH(syscall);
 
-int hello(struct bpf_raw_tracepoint_args *ctx){
+RAW_TRACEPOINT_PROBE("sys_enter"){
     u64 uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
     u64 counter = 0;
     u64 *p;
@@ -21,11 +21,6 @@ int hello(struct bpf_raw_tracepoint_args *ctx){
 """
 
 b = BPF(text=program)
-
-prog_array = b.get_table("syscall")
-
-b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
-
 while True:
     time.sleep(2)
     s=""
