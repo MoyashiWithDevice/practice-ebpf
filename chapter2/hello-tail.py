dd54ecf6458c6859fff7491cf4b82d004cd99ab1
bababa <pijonsan@gmail.com>
2026-05-07 17:54:42 +0900

    syscallにアタッチするebpfを、システムコールの回数をユーザごとに表示するように変更

--- chapter2/hello-tail.py
+++ chapter2/hello-tail.py
@@ -1,55 +1,34 @@
 #!/usr/bin/python3
 from bcc import BPF
-import ctypes as ct
+import time
 
 program = r"""
 BPF_PROG_ARRAY(syscall, 300);
 
 int hello(struct bpf_raw_tracepoint_args *ctx){
-    int opcode = ctx -> args[1];
-    syscall.call(ctx, opcode);
-    bpf_trace_printk("Another syscall: %d", opcode);
-    return 0;
-}
-int hello_execve(void *ctx){
-    bpf_trace_printk("Executing a program");
-    return 0;
-}
+    u64 uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+    u64 counter = 0;
+    u64 *p;
 
-int hello_timer(struct bpf_raw_tracepoint_args *ctx){
-    if (ctx->args[1] == 222){
-        bpf_trace_printk("Creating a timer");
-    }else if (ctx->args[1] == 226){
-        bpf_trace_printk("Deleting a timer");
-    }else{
-        bpf_trace_printk("Some other timer operation");
+    p = syscall.lookup(&uid);
+    if (p != 0){
+        counter = *p;
     }
-    return 0;
-}
-int ignore_opcode(void *ctx){
+    counter++;
+    syscall.update(&uid, &counter);
     return 0;
 }
 """
 
 b = BPF(text=program)
 
-ignore_fn = b.load_func("ignore_opcode", BPF.RAW_TRACEPOINT)
-execve_fn = b.load_func("hello_execve", BPF.RAW_TRACEPOINT)
-timer_fn = b.load_func("hello_timer", BPF.RAW_TRACEPOINT)
-
 prog_array = b.get_table("syscall")
 
-prog_array[ct.c_int(59)] = ct.c_int(execve_fn.fd)
-prog_array[ct.c_int(222)] = ct.c_int(timer_fn.fd)
-prog_array[ct.c_int(223)] = ct.c_int(timer_fn.fd)
-prog_array[ct.c_int(224)] = ct.c_int(timer_fn.fd)
-prog_array[ct.c_int(225)] = ct.c_int(timer_fn.fd)
-prog_array[ct.c_int(226)] = ct.c_int(timer_fn.fd)
-
-prog_array[ct.c_int(1)] = ct.c_int(ignore_fn.fd)
-prog_array[ct.c_int(21)] = ct.c_int(ignore_fn.fd)
-prog_array[ct.c_int(22)] = ct.c_int(ignore_fn.fd)
-prog_array[ct.c_int(25)] = ct.c_int(ignore_fn.fd)
-
 b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
-b.trace_print()
+
+while True:
+    time.sleep(2)
+    s=""
+    for k,v in b["syscall"].items():
+        s += f"ID {k.value}: {v.value}\t"
+        print(s)
